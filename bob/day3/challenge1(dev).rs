bd43e86c9e77788d4dd2825e2b1c8f6ef69e2c8e
Bobur <bobs4462@gmail.com>
2019-11-28 02:43:50 +0300

    some cleanup

---
+++ bob/day3/challenge1(dev).rs
@@ -0,0 +1,54 @@
+use std::fs::File;
+use std::io::Read;
+
+struct Patch {
+    x: usize,
+    y: usize,
+    width: usize,
+    height: usize,
+}
+
+fn parse(data: &str) -> Vec<Patch> {
+    let mut patches: Vec<Patch> = Vec::new();
+    let mut lines = data.split('\n').collect::<Vec<&str>>();
+    lines.pop().unwrap();
+
+    for l in lines {
+        let t = l
+            .split(|c: char| !c.is_digit(10))
+            .filter(|i| i.len() > 0)
+            .map(|i| i.parse().unwrap())
+            .collect::<Vec<usize>>();
+        patches.push(Patch {
+            x: t[1],
+            y: t[2],
+            width: t[3],
+            height: t[4],
+        })
+    }
+    patches
+}
+
+fn main() {
+    let mut count = 0;
+    let mut f = File::open("data.txt").expect("filesystem error");
+    let mut s: String = String::new();
+    f.read_to_string(&mut s).expect("io error");
+    let patches = parse(&s);
+    let mut inches = vec![vec![0; 1000]; 1000];
+    for p in patches {
+        for y in p.y + 1..p.y + p.height + 1 {
+            for x in p.x + 1..p.x + p.width + 1 {
+                inches[y][x] += 1;
+                //if inches[y][x] != 2 {
+                //    if inches[y][x] >= 2 {
+                //        inches[y][x] = 2;
+                //        count += 1;
+                //    }
+                //}
+            }
+        }
+    }
+    count = inches.into_iter().flatten().filter(|i| i > &1).count();
+    println!("count {}", count);
+}
--- bob/day3/challenge1.rs
+++ bob/day3/challenge1.rs
@@ -2,19 +2,21 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
+// patch of space defined by x and y coordinates and width and height
 struct Patch {
     x: u32,
     y: u32,
     width: u32,
     height: u32,
 }
-
-#[derive(Hash, Eq, PartialEq)]
+// for storing cooridnates of point
+#[derive(Hash, Eq, PartialEq)] //in order to use Inch as hashmap key
 struct Inch {
     x: u32,
     y: u32,
 }
 
+// function to retrive a vector of patches
 fn parse(data: &str) -> Vec<Patch> {
     let mut patches: Vec<Patch> = Vec::new();
     let mut lines = data.split('\n').collect::<Vec<&str>>();
@@ -40,8 +42,8 @@ fn main() {
     let mut f = File::open("data.txt").expect("filesystem error");
     let mut s: String = String::new();
     f.read_to_string(&mut s).expect("io error");
-    let patches = parse(&s);
-    let mut inches: HashMap<Inch, u32> = HashMap::new();
+    let patches = parse(&s); // parsing string
+    let mut inches: HashMap<Inch, u32> = HashMap::new(); // to store all the points encountered
     for p in patches {
         for x in p.x + 1..p.x + p.width + 1 {
             for y in p.y + 1..p.y + p.height + 1 {
