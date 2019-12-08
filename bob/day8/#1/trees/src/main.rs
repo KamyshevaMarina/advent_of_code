b2ed7781b2c238b7adefa31fd5718651f0877411
Bobur <bobs4462@gmail.com>
2019-12-09 00:40:35 +0300

    day 8

---
+++ bob/day8/#1/trees/src/main.rs
@@ -0,0 +1,30 @@
+use std::cell::RefCell;
+use std::fs;
+use std::rc::Rc;
+
+fn main() {
+    let mut list: Vec<u32> = fs::read_to_string("data.txt")
+        .unwrap()
+        .split('\n')
+        .filter_map(|i| i.parse::<u32>().ok())
+        .collect();
+    list.reverse();
+    let list = Rc::new(RefCell::new(list));
+    let i = meta_get(list.clone());
+    println!("RESULT: {}", i);
+}
+
+fn meta_get(list: Rc<RefCell<Vec<u32>>>) -> u32 {
+    let count = list.borrow_mut().pop().unwrap();
+    let meta_count = list.borrow_mut().pop().unwrap();
+    let mut meta_sum = 0;
+    if count > 0 {
+        for _ in 0..count {
+            meta_sum += meta_get(list.clone());
+        }
+    }
+    for _ in 0..meta_count {
+        meta_sum += list.borrow_mut().pop().unwrap();
+    }
+    meta_sum
+}
