461c348f83b7f1c2377a4b2be44be6d1913c4ea2
Ulanovich_Ilya <36299101+ilyuxa-sw97@users.noreply.github.com>
2019-12-15 17:11:43 +0300

    123

---
+++ ilyuxa/day7/day7.2.py
@@ -0,0 +1,93 @@
+# https://adventofcode.com/2018/day/7
+import re
+import string
+
+
+class Point_tree():
+    def __init__(self, point, list_point_before, list_point_after):
+        self.point = point
+        self.list_point_before = sorted(list_point_before)
+        self.list_point_after = sorted(list_point_after)
+
+
+def return_clas_list(list_point, a_before_b_list):  # возвращает списко объектов класа для каждой точки
+    list_point_before, list_point_after, list_class_point = [], [], {}
+    for i in list_point:
+        for j in a_before_b_list:
+            if i == j[0]:
+                list_point_after.append(j[1])
+            elif i == j[1]:
+                list_point_before.append(j[0])
+        list_class_point[i] = (Point_tree(i, list_point_before, list_point_after))
+        list_point_before, list_point_after = [], []
+    return list_class_point
+
+
+def list_st(listst):
+    listst = list(set(listst))
+    listst = sorted(listst)
+    return listst
+
+
+def main():
+    d, allpoint, list_class_point, point_now, answer, list_statement, unclaimed = [], [], {}, '', '', [], []
+    with open("num.txt") as file_handler:
+        for line in file_handler:
+            d.append([line[5], line[36]])
+            allpoint.append(line[5])
+            allpoint.append(line[36])
+    allpoint = set(allpoint)
+    list_class_point = return_clas_list(allpoint, d)
+    for i in list_class_point.items():
+        if i[1].list_point_before == []:
+            list_statement.append(i[0])
+    time_point = {i: j+60 for i, j in zip(string.ascii_uppercase, range(1, len(string.ascii_uppercase) + 1))}
+    step, len_worker, time_now, = 0, {i: [0, 0] for i in range(5)}, -2
+
+    while True:
+        time_now += 1
+        for i in range(len(len_worker)):
+            if len_worker[i][0] == 0 and list_statement != []:
+                list_statement = list_st(list_statement)
+                point_now = list_statement.pop(0)
+                if set(list_class_point[point_now].list_point_before).issubset(answer) or answer == '':
+                    len_worker[i][0] = point_now
+                else:
+                    unclaimed.append(point_now)
+            elif len_worker[i][0] != 0:
+                len_worker[i][1] += 1
+                if len_worker[i][1] == time_point[len_worker[i][0]]:
+                    answer += len_worker[i][0]
+                    list_statement = list_statement + list_class_point[len_worker[i][0]].list_point_after + unclaimed
+                    unclaimed = []
+                    list_statement = list_st(list_statement)
+                    len_worker[i] = [0, 0]
+                    if list_statement!=[]:
+                        if set(list_class_point[list_statement[0]].list_point_before).issubset(answer):
+                            len_worker[i][0] = list_statement.pop(0)
+        for i in range(len(len_worker)):
+            if len_worker[i][0] == 0 and list_statement != []:
+                list_statement = list_st(list_statement)
+                if set(list_class_point[list_statement[0]].list_point_before).issubset(answer):
+                    len_worker[i][0] = list_statement.pop(0)
+                else:
+                    unclaimed.append(list_statement.pop(0))
+        for i in range(len(len_worker)):
+            if len_worker[i][0] == 0 and list_statement != []:
+                list_statement = list_st(list_statement)
+                if set(list_class_point[list_statement[0]].list_point_before).issubset(answer):
+                    len_worker[i][0] = list_statement.pop(0)
+                else:
+                    unclaimed.append(list_statement.pop(0))
+        #print(len_worker)
+
+
+        if len(answer) >= len(allpoint):
+            break
+    print(answer)
+    print(time_now)
+    print('EUGJKYFQSCLTWXNIZMAPVORDBH')
+
+
+if __name__ == "__main__":
+    main()
